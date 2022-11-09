83fd789c86ce87e1baa7f9130c2975d4180e5017
Andrea <andrea@kaist.ac.kr>
2022-11-09 15:58:34 +0900

    Better example

--- main.py
+++ main.py
@@ -8,15 +8,11 @@ I2C_ADDR = 0x27
 totalRows = 4
 totalColumns = 16
 
-print(i2c.scan())
 i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400_000)
+print(i2c.scan()) # 0x27
 lcd = I2cLcd(i2c, I2C_ADDR, totalRows, totalColumns)
 
-while True:
-    lcd.putstr("Lets Count 0-10!")
-    sleep(2)
-    lcd.clear()
-    for i in range(10):
-        lcd.putstr(str(i))
-        sleep(1)
-        lcd.clear()
+lcd.clear()
+lcd.putstr("Hello\nWorld")
+
+
